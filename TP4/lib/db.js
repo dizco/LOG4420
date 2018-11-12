a22d8c39f7b4a171b45872f9b3806771f8298b85
Gabriel <dizco@users.noreply.github.com>
2018-11-12 12:03:40 -0500

    Extract mongoose schemas and models

--- TP4/lib/db.js
+++ TP4/lib/db.js
@@ -1,32 +1,10 @@
 "use strict";
 
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const Order = new Schema({
-  id: { type: Number, unique: true },
-  firstName: String,
-  lastName: String,
-  email: String,
-  phone: String,
-  products: Array
-}, { versionKey: false });
-
-
-const Product = new Schema({
-  id: { type: Number, unique: true },
-  name: String,
-  price: Number,
-  image: String,
-  category: String,
-  description: String,
-  features: Array
-}, { versionKey: false });
-
-mongoose.model("Order", Order);
-mongoose.model("Product", Product);
+require("./order");
+require("./product");
 
 mongoose.Promise = global.Promise;
 
-// TODO: Initialiser la connexion avec le "connect string" de votre base de données.
-//mongoose.connect("mongodb://...", { useMongoClient: true });
+mongoose.connect("mongodb://pikachu:2s#Mbx1xZ#6gtSsO!*XG@ds159563.mlab.com:59563/online-shop", { useMongoClient: true });
---
+++ TP4/lib/order.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Order = new Schema({
+  id: { type: Number, unique: true },
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  products: Array
+}, { versionKey: false });
+
+module.exports = mongoose.model("Order", Order);
