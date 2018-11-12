6bf392bf99a334cb6cad8966089a0f48dd2e7af1
Gabriel <dizco@users.noreply.github.com>
2018-11-12 12:04:06 -0500

    Draft products controller
    Must add validations all around

--- TP4/routes/products.js
+++ TP4/routes/products.js
@@ -1,29 +1,59 @@
 const express = require("express");
 const router = express.Router();
 
+const Product = require("../lib/product");
+
 //Get all products
 router.get("/", (req, res) => {
-  res.json({});
+  Product.find({})
+    .then((products) =>  res.json(products))
+    .catch((err) => {
+      console.error("Error executing mongoose", err);
+      res.json({success: false, error: err})
+    });
 });
 
 //Get product by id
 router.get("/:id", (req, res) => {
-  res.json({});
+  Product.find({id: req.params.id})
+    .then((products) =>  res.json(products))
+    .catch((err) => {
+      console.error("Error executing mongoose", err);
+      res.json({success: false, error: err})
+    });
 });
 
 //Create new product
 router.post("/", (req, res) => {
-  res.json({});
+  let product = new Product({
+    ...req.body
+  });
+  product.save((err) => {
+    if (err) {
+      throw err;
+    }
+    res.json({success: true});
+  });
 });
 
 //Delete product by id
 router.delete("/:id", (req, res) => {
-  res.json({});
+  Product.findOneAndRemove({id: req.params.id})
+    .then(() => res.json({success: true}))
+    .catch((err) => {
+      console.error("Error executing mongoose", err);
+      res.json({success: false, error: err})
+    });
 });
 
 //Delete all products
-router.get("/", (req, res) => {
-  res.json({});
+router.delete("/", (req, res) => {
+  Product.remove({})
+    .then(() => res.json({success: true}))
+    .catch((err) => {
+      console.error("Error executing mongoose", err);
+      res.json({success: false, error: err})
+    });
 });
 
 module.exports = router;
